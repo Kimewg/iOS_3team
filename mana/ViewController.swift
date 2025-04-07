316bc2ccf2e36f867f7c1e5c57b335ef20880fe7
eunseo <neruneru33@naver.com>
2025-04-07 17:34:23 +0900

    new

---
+++ mana/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mana
+//
+//  Created by 김은서 on 4/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
