18c1e2cddbe0bd6f44c74aa33e1c8bcc2b92d4b4
Sophie4han <hanshirley775@gmail.com>
2025-04-08 22:31:21 +0900

    cell UI 속성 및 레이아웃 초기 구현

--- mana/MangaCollectionViewCell.swift
+++ mana/MangaCollectionViewCell.swift
@@ -6,7 +6,44 @@
 //
 
 import UIKit
+import SnapKit
 
 class MangaCollectionViewCell: UICollectionViewCell {
+    let imageView = UIImageView()
+    let infoBackgroundView = UIView()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupMangaCollection()
+        setupCellLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupMangaCollection() {
+        [imageView,infoBackgroundView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview($0)
+        }
+        imageView.layer.borderWidth = 1
+        imageView.contentMode = .scaleAspectFill //이미지 잘리지 않고 크기 유지
+
+//        imageView.image = UIImage(named: "manga_sample_1")
+        
+        infoBackgroundView.backgroundColor = UIColor.black.withAlphaComponent(0.7)
+        infoBackgroundView.layer.cornerRadius = 30
+        infoBackgroundView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMaxYCorner, .layerMaxXMaxYCorner)
+    }
+    func setupCellLayout() {
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview())
+        }
+        infoBackgroundView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalTo(imageView)
+            make.height.equalTO(55)
+        }
+    }
 }
+
