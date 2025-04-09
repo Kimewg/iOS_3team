d8c4e44921b96b48c705ea1eb82d056c2070adc2
eunseo <neruneru33@naver.com>
2025-04-09 19:49:21 +0900

    test

--- mana/AppDelegate.swift
+++ mana/AppDelegate.swift
@@ -1,14 +1,16 @@
 //
 //  AppDelegate.swift
-//  mana
+//  Mana
 //
-//  Created by 김은서 on 4/7/25.
+//  Created by 김은서 on 4/9/25.
 //
 
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- mana/Scroll, Stack/ButtonUI.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-func ButtonUI(title: String) -> UIButton{
-    let button = UIButton(type: .system)
-    button.setTitle(title, for: .normal)
-    return button
-}
